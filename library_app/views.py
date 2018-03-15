9df3ee2bdd02c946fddfee9276e7486ed0bbbdff
Arman <meirambek.arman.97@gmail.com>
2018-03-15 18:17:21 +0600

    #4 Дата и Время костым.

--- library_app/views.py
+++ library_app/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render, get_object_or_404
 from library_app.models import Category, Book, Publisher, Author
+import datetime
 # Create your views here.
 
 def home(request):
     book_categories = Category.objects.all()
+
     # Number of visits to this view, as counted in the session variable.
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-    return render(request, 'home.html', {'categories':book_categories,'num_visits':num_visits})
+
+    time = datetime.datetime.now()
+    w_day = time.weekday()
+    day = time.day
+    month = time.month
+    year = time.year
+    microsecond = time.microsecond
+    second = time.second
+    minute = time.minute
+    hour = time.hour
+
+    return render(request, 'home.html', {'categories':book_categories,'num_visits':num_visits, 'weekday': w_day,
+                 'hour': hour, 'minute': minute, 'second': second, 'microsecond': microsecond,
+                 'day': day, 'month': month, 'year': year})  # num_visits appended
 
 def book_list_in_category(request, pk):
     category = get_object_or_404(Category, pk=pk)
